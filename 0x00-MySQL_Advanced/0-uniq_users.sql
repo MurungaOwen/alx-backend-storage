8e87eb43b4c0a4661581193107ceb5799e3acf2f
MurungaOwen <owenhood80@gmail.com>
2024-03-21 23:58:00 +0300

    checker

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,3 @@
--- script to create a table for users
--- create users table with id email and name
-CREATE TABLE IF NOT EXISTS users(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,email VARCHAR(255) NOTNULL, name VARCHAR(255));
+-- script to create a table users
+-- create table users with attribute id email name
+CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255));
