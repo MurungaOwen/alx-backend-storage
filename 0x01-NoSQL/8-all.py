4d72fafdf987f698973ba1f49ed70e872d894f0c
MurungaOwen <owenhood80@gmail.com>
2024-03-26 08:45:52 +0300

    format doc

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
-def list_all(mongo_collection):
-    pass
+from typing import List
+
+
+def list_all(mongo_collection) -> List:
+    """a script to list objects in collect
+    ion
+    params:
+        mongo_collection as collection
+    return:
+        list of documents
+    """
+    result = mongo_collection.find()
+    return result
