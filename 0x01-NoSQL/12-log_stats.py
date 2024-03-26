525c16ced04fd76228033f191e010e693809bd5f
MurungaOwen <owenhood80@gmail.com>
2024-03-26 18:14:09 +0300

    lint 12-log

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,28 +5,24 @@ list_all = __import__('8-all').list_all
 
 
 client = MongoClient('mongodb://127.0.0.1:27017')
-
 db = client["logs"]
 collection = db.get_collection("nginx")
+
 
 def get_total() -> int:
     """return total contents"""
     return collection.count_documents({})
+
 
 def methods_total() -> int:
     """return total /status"""
     method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods: ")
     for x in method:
-        total=collection.count_documents({"method":x})
+        total = collection.count_documents({"method": x})
         print(f'    method {x}: {total}')
     return f'{collection.count_documents({"path": "/status"})} status check'
+
+
 print(f'{get_total()} logs')
 print(methods_total())
-
-
-
-
-
-
-
