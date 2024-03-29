b5a7ac9a5a793dfbed8f6bac91e3cc37ea14a54e
MurungaOwen <owenhood80@gmail.com>
2024-03-29 16:16:51 +0300

    web

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,30 @@
+from typing import Callable
+from functools import wraps
+import redis
+import requests
+redis = redis.Redis()
+
+
+def wrap_requests(fn: Callable) -> Callable:
+    """ Decorator wrapper """
+
+    @wraps(fn)
+    def wrapper(url):
+        """ Wrapper for decorator guy """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
+        return result
+
+    return wrapper
+
+
+@wrap_requests
+def get_page(url: str) -> str:
+    """get page self descriptive
+    """
+    response = requests.get(url)
+    return response.text
